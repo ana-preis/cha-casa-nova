766ba46b00c87a7dba1402326481b73b76ee2c3e
AnaPreis <anarps_@hotmail.com>
2023-11-19 20:44:18 -0300

    feat: kv connection with try catch

--- api/kv.js
+++ api/kv.js
@@ -10,37 +10,37 @@ require("node-fetch")
 
 module.exports = async (req, res) => {
 
-  // const kvClient = kv.createClient({
-  //   url: process.env.KV_REST_API_URL,
-  //   token: process.env.KV_REST_API_TOKEN,
-  // })
-  console.log('url: ', process.env.KV_REST_API_URL)
-  console.log('token: ', process.env.KV_REST_API_TOKEN)
-  const { KV_REST_API_URL, KV_REST_API_TOKEN } = process.env;
+  const kvClient = kv.createClient({
+    url: process.env.KV_REST_API_URL,
+    token: process.env.KV_REST_API_TOKEN,
+  })
+  // console.log('url: ', process.env.KV_REST_API_URL)
+  // console.log('token: ', process.env.KV_REST_API_TOKEN)
+  // const { KV_REST_API_URL, KV_REST_API_TOKEN } = process.env;
 
 
   if (req.method === 'GET') {
     console.log('dentro do get')
-    fetch(`${KV_REST_API_URL}/`, {
-      headers: {
-        Authorization: `Bearer ${KV_REST_API_TOKEN}`,
-      },
-      body: '["HGETALL", "gift:list"]',
-      method: 'POST',
-    })
-      .then((response) => response.json())
-      .then((data) => console.log('backend response: ',  data));
-    // try {
-    //   console.group(' dentro do try: ')
-    //   // const giftList = await kvClient.hgetall("gift:list");
-    //   const giftList = await kvClient.hget("gift:list", "1");
-    //   // return res.status(200).json(giftList);
-    //   console.log(giftList)
-    //   console.groupEnd()
-    //   return res.status(200).json({sdf:" sdfd"});
-    // } catch (error) {
-    //   return res.status(500).json({err:"erro"})
-    // }
+    // fetch(`${KV_REST_API_URL}/`, {
+    //   headers: {
+    //     Authorization: `Bearer ${KV_REST_API_TOKEN}`,
+    //   },
+    //   body: '["HGETALL", "gift:list"]',
+    //   method: 'POST',
+    // })
+    //   .then((response) => response.json())
+    //   .then((data) => console.log('backend response: ',  data));
+    try {
+      console.group('dentro do try')
+      // const giftList = await kvClient.hgetall("gift:list");
+      const giftList = await kvClient.hgetall("gift:list");
+      // return res.status(200).json(giftList);
+      console.log(giftList)
+      console.groupEnd()
+      return res.status(200).json(giftList);
+    } catch (error) {
+      return res.status(500).json({err:"erro: "})
+    }
   // } else {
   //   const data = req.body;
   //   await kvClient.set("gift:list", data.index, data)
